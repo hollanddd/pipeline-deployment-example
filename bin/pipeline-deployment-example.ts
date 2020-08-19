a35027e7672f4ae0cca25ae242f94202c767fdaf
hollanddd <me@darrenholland.com>
2020-08-19 13:37:11 -0500

    Declare stacks in the same place

--- bin/pipeline-deployment-example.ts
+++ bin/pipeline-deployment-example.ts
@@ -3,5 +3,21 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { PipelineDeploymentExampleStack } from '../lib/pipeline-deployment-example-stack';
 
+import { PipelineStack } from '../lib/pipeline-stack';
 const app = new cdk.App();
 new PipelineDeploymentExampleStack(app, 'PipelineDeploymentExampleStack');
+
+new PipelineStack(app, 'ExamplePipelineStack', {
+  repo: {
+    owner:  app.node.tryGetContext('owner'),
+    name:   app.node.tryGetContext('reponame'),
+    branch: app.node.tryGetContext('branch'),
+  },
+  env: {
+    account: cdk.Aws.ACCOUNT_ID,
+    region: cdk.Aws.REGION,
+ },
+});
+
+app.synth();
+
