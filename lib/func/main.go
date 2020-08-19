1936dab51b409edca6e1e2dbb4fe9938310071c5
hollanddd <me@darrenholland.com>
2020-08-19 13:22:30 -0500

    Add lambda function example in Go runtime

---
+++ lib/func/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+)
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       "Hello from CDK Go Function!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- lib/pipeline-deployment-example-stack.ts
+++ lib/pipeline-deployment-example-stack.ts
@@ -1,9 +1,24 @@
+import * as golang from 'aws-lambda-golang';
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class PipelineDeploymentExampleStack extends cdk.Stack {
+  public readonly urlOutput: cdk.CfnOutput;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new golang.GolangFunction(this, 'func'); 
+
+    const api = new apigateway.LambdaRestApi(this, 'myapi', {
+      handler,
+      proxy: false,
+    });
+
+    const items = api.root.addResource('hello');
+    items.addMethod('GET');
+
+    this.urlOutput = new cdk.CfnOutput(this, 'ApiURL', {
+      value: api.url,
+    });
   }
 }
